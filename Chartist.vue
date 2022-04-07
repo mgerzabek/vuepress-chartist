661525b464c9e14ed4730ce7687b49e8b40fb93f
Michael Gerzabek <michael.gerzabek@gmail.com>
2022-04-07 13:51:52 +0200

    initial version

---
+++ Chartist.vue
@@ -0,0 +1,10 @@
+<template>
+  <script>
+    const charts = document.getElementsByClassName('ct-chart');
+    for (let chart of charts)
+    {
+      const json = JSON.parse(chart.nextSibling.innerText);
+      new Chartist.Line(chart, json.data, json.option);
+    }
+  </script>
+</template>
---
+++ enhanceAppFile.js
@@ -0,0 +1,6 @@
+import Chartist from './Chartist.vue'
+
+export default ({ Vue }) => {
+  // eslint-disable-next-line vue/match-component-file-name
+  Vue.component('Chartist', Chartist)
+}
